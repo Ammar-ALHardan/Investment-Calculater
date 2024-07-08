a09458145a409133b79cf8fe575fc500d1c70c6d
Ammar-ALHardan <ammarhardan863@gmail.com>
2024-07-08 11:33:35 +0300

    using signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
@@ -15,14 +15,23 @@ import { InvestmentResultsComponent } from "./investment-results/investment-resu
 export class AppComponent {
   title = 'Investment-Calculater';
 
-  resultsData?: {
+  // resultsData?: {
+  //   year: number,
+  //   interest: number,
+  //   valueEndOfYear: number,
+  //   annualInvestment: number,
+  //   totalInterest: number,
+  //   totalAmountInvested: number,
+  // }[];
+
+  resultsData = signal<{
     year: number,
     interest: number,
     valueEndOfYear: number,
     annualInvestment: number,
     totalInterest: number,
     totalAmountInvested: number,
-  }[]
+  }[] | undefined >(undefined)
 
   onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment , annualInvestment , expectedReturn , duration } = data;  
@@ -45,7 +54,10 @@ export class AppComponent {
       });
     }
   
-    this.resultsData = annualData;
+    // this.resultsData = annualData;
+
+    // when using signal
+    this.resultsData.set(annualData)
     
   }
 }
